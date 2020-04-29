87dc9cfc4da5cd36f667cf27507c5caec860dcc7
karim <karim@csit.fi>
2020-04-30 00:22:16 +0300

    digit component created

---
+++ src/components/countdown/digit.jsx
@@ -0,0 +1,17 @@
+ 
+import React from 'react'
+
+const Digit = (props) =>
+ {
+     return(
+         <div className="Digit mx-2 text-center my-4" style = { { borderColor:props.color }}>
+         <h1 className="display-2  py-3" style = { { color:props.color }}>
+
+         {props.value < 10 ? `0${props.value}` : props.value}</h1>
+         </div>
+     )
+
+
+ }
+
+ export default Digit
