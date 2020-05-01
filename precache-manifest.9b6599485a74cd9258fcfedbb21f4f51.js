b4f32124a67bb91508384c3fd2ddf4271296c32e
karim <karim@csit.fi>
2020-05-01 04:06:05 +0300

    Updates

---
+++ precache-manifest.9b6599485a74cd9258fcfedbb21f4f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fb3316f0dda0e633b12b5bdf29e2b5",
+    "url": "/countdown/index.html"
+  },
+  {
+    "revision": "80a2aaae74d731dfb000",
+    "url": "/countdown/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a0b7ed87503a74668765",
+    "url": "/countdown/static/css/main.604f92eb.chunk.css"
+  },
+  {
+    "revision": "80a2aaae74d731dfb000",
+    "url": "/countdown/static/js/2.e6d179b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown/static/js/2.e6d179b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b7ed87503a74668765",
+    "url": "/countdown/static/js/main.d814338a.chunk.js"
+  },
+  {
+    "revision": "699dad5d2fd630d6c8fc",
+    "url": "/countdown/static/js/runtime-main.fac0ac1a.js"
+  }
+]);
